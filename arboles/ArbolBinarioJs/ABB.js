6337638e40c811fcdd883d0bf5c25d987a5c2374
KritianWhite <3617836980101@ingenieria.usac.edu.gt>
2022-06-14 01:14:13 -0600

    ABB whit dot graphviz

--- arboles/ArbolBinarioJs/ABB.js
+++ arboles/ArbolBinarioJs/ABB.js
@@ -19,6 +19,7 @@ class ArbolABB {
   }
 
   agregar(id, node = this.raiz) {
+    id = id.replace(/ /g, "_")
     if (!node) {
       this.raiz = new Nodo(id);
       return;
@@ -65,9 +66,41 @@ class ArbolABB {
     console.log(node.id);
     this.InOrden(node.derecha);
   }
+
+  graficar(raiz) {
+    var cadena = '';
+    cadena += "digraph G { \n"
+    cadena +="rankdir=TB; \n";
+    cadena += "node [shape = record, color=black , style=filled, fillcolor=gray93];\n";
+    cadena += this.__graficadora(raiz);
+    cadena += "} \n";
+    d3.select("#lienzo").graphviz().width(1350).height(500).renderDot(cadena);
+  }
+
+  __graficadora(root) {
+    var cadena;
+    cadena = "";
+
+    if (root.derecha === null && root.izquierda === null) {
+      cadena = "nodo" + root.id.toString() + "[label =\" " + root.id.toString().replace(/_/g, " ") + "\"]; \n";
+    } else {
+      cadena = "nodo" + root.id.toString() + "[label =\"<C0>| " + root.id.toString().replace(/_/g, " ") + "|<C1> \"]; \n";
+    }
+
+    if (root.izquierda !== null) {
+      cadena = cadena + this.__graficadora(root.izquierda) + "nodo" + root.id.toString() + ":C0->nodo" + root.izquierda.id.toString() + "\n";
+    }
+
+    if (root.derecha !== null) {
+      cadena = cadena + this.__graficadora(root.derecha) + "nodo" + root.id.toString() + ":C1->nodo" + root.derecha.id.toString() + "\n";
+    }
+    return cadena
+  }
+
 }
 
-arbolito = new ArbolABB();
+let arbolito = new ArbolABB();
+/*
 arbolito.agregar(15);
 arbolito.agregar(5);
 arbolito.agregar(3);
@@ -80,7 +113,17 @@ arbolito.agregar(16);
 arbolito.agregar(20);
 arbolito.agregar(18);
 arbolito.agregar(23);
+*/
+
+arbolito.agregar("Alice Kellen");
+arbolito.agregar("John le Carré");
+arbolito.agregar("Aba Frank");
+arbolito.agregar("Sthephenie Meyer");
+arbolito.agregar("Paulo Coelho");
+arbolito.agregar("Martha Cerda");
+
 
 arbolito.preOrden(arbolito.raiz);
 arbolito.postOrden(arbolito.raiz);
 arbolito.InOrden(arbolito.raiz);
+arbolito.graficar(arbolito.raiz);
